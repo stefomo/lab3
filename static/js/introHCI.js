b37f9460d5990d796352e98f32ea70445c09d8ae
Stefanie Mendoza <svm005@ucsd.edu>
2020-01-23 12:31:31 -0800

    added JS

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -5,14 +5,33 @@ $(document).ready(function() {
 	initializePage();
 })
 
+
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("I hope it's working");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").addClass("active");
 	});
+	$("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) {
+	e.preventDefault();
+	$(".project-description").fadeOut();
+
+	
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
-}
+	
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
+ }
